f5180464e9b178766c8f940a6158aa5d2f514984
devDataEngineer <oleg.dachcenter@gmail.com>
2024-05-31 20:28:19 +0100

    Initial commit

---
+++ research.py
@@ -0,0 +1,20 @@
+'''first=input('Enter a first number ')
+second=input('Enter a second number ')
+result = int(first) + int(second)
+print(result)
+course= 'Pythin for Beginners'''
+
+
+weight=float(input('Weight? '))
+measurement=input('(K)g or (L)bs? ').upper()
+result = 0.0
+if measurement == 'K':
+    result = weight*2.2
+    print(f'Weight in Lbs is {result}')
+elif measurement == 'L':
+    result = weight*0.45
+    print(f'Weight in Kg is {result}')
+else: 
+    print ('You have provided incorrect input')
+    
+    
